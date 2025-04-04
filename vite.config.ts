72f22ae8fd11a6eb4d45e1249e4d97f0c93b724b
Henning Pohlmeyer <hpohlmeyer@users.noreply.github.com>
2025-04-04 11:42:45 +0200

    Use vitest browser mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: "playwright",
+      instances: [{ browser: "chromium" }],
+    },
+  },
 })
